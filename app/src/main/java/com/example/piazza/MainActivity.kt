03b2e75f60ca3219dbd2642fd8905183accbdbe1
DGP115 <david.g.peloso@gmail.com>
2026-02-17 23:34:10 -0500

    Minimal toolbar on Android

--- app/src/main/java/com/example/piazza/MainActivity.kt
+++ app/src/main/java/com/example/piazza/MainActivity.kt
@@ -2,16 +2,59 @@ package com.example.piazza
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ViewFlipper
+import androidx.activity.viewModels
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.hotwire.turbo.activities.TurboActivity
 import dev.hotwire.turbo.delegates.TurboActivityDelegate
 
 class MainActivity : AppCompatActivity(), TurboActivity {
     override lateinit var delegate: TurboActivityDelegate
+    lateinit var tabBar: BottomNavigationView
+
+    private lateinit var tabSwitcher: ViewFlipper
+
+    /*
+    The below line lazily instantiates the tabsViewModel using the Android
+    platform. Android’s ViewModel class does some lifecycle management behind the scenes
+    so we can just ask the system for one instead of instantiating it ourselves.
+     */
+    private val tabsViewModel: TabsViewModel by viewModels()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        delegate = TurboActivityDelegate(this, R.id.main_nav_host)
+        // Register each tab's SessionNavHostFragment with Turbo Native
+        configureTurboDelegates()
+
+        // Handle logic for switching between tabs
+        configureTabs()
+
+    }
+
+    private fun configureTurboDelegates() {
+        delegate =
+            TurboActivityDelegate(this, tabsViewModel.tabs.first().id)
+        tabsViewModel.tabs.forEach {
+            delegate.registerNavHostFragment(it.id)
+        }
+    }
+
+    private fun configureTabs() {
+        tabSwitcher = findViewById(R.id.tabSwitcher)
+        tabBar = findViewById(R.id.tabBar)
+
+        tabBar.setOnItemSelectedListener {
+            tabSwitcher.displayedChild =
+                tabsViewModel.indexedTabForId(it.itemId)!!.index
+
+            delegate.currentNavHostFragmentId = it.itemId
+            delegate.refresh(false)
+
+            return@setOnItemSelectedListener true
+        }
     }
 }
--- app/src/main/java/com/example/piazza/SessionNavHostFragment.kt
+++ app/src/main/java/com/example/piazza/SessionNavHostFragment.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import dev.hotwire.turbo.config.TurboPathConfiguration
 import dev.hotwire.turbo.session.TurboSessionNavHostFragment
 import kotlin.reflect.KClass
+import android.os.Bundle
+import androidx.fragment.app.activityViewModels
 
 class SessionNavHostFragment : TurboSessionNavHostFragment() {
-    override var sessionName = "main"
-    override var startLocation = Api.rootUrl
+    override var sessionName = "default"
+   // override var startLocation = Api.rootUrl
+    override var startLocation = "https://google.com"
+
+    val tabsViewModel: TabsViewModel by activityViewModels()
+
     override val registeredActivities:
             List<KClass<out AppCompatActivity>>
         get() = listOf()
+
     override val registeredFragments:
             List<KClass<out Fragment>>
         get() = listOf(
             WebFragment::class
         )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        sessionName = "tab_$tag"
+        tabsViewModel.tabForId(id)?.url?.let { startLocation = it }
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onSessionCreated() {
         super.onSessionCreated()
         session.webView.settings.userAgentString =
@@ -26,5 +40,5 @@ class SessionNavHostFragment : TurboSessionNavHostFragment() {
             TurboPathConfiguration.Location
         get() = TurboPathConfiguration.Location(
             assetFilePath = "json/configuration.json"
-        )
+       )
 }
