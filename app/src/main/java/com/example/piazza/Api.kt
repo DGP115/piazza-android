02a0a615180bef24eebdfe4d731a2a3c20b8dc4c
David or Margarita Peloso <david.g.peloso@gmail.com>
2026-03-20 14:23:32 -0400

    Native app working

--- app/src/main/java/com/example/piazza/Api.kt
+++ app/src/main/java/com/example/piazza/Api.kt
@@ -2,9 +2,10 @@ package com.example.piazza
 
 class Api {
     companion object {
-        // Replace 192.168.1.1 with your computer's actual IPv4 address
+        // When running this app on your Android phone, replace 192.168.*.*
+        // with your computer's actual IPv4 address
         private val baseUrl = if(BuildConfig.DEBUG)
-            "http://192.168.68.113:3000"
+            "http://192.168.68.103:3000"
         else
             "https://piazza-web-61uo.onrender.com"
         val rootUrl = "$baseUrl/"
